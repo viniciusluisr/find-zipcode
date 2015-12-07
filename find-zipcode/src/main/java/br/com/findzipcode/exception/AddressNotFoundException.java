7f6cde56cb6ce226deb1d2234cb3ac2a3c11fbee
Vinícius Rodrigues <vinicius.rodrigues@icolabora.com.br>
2015-12-07 11:47:36 -0200

    Ajustes parciais

--- find-zipcode/src/main/java/br/com/findzipcode/exception/AddressNotFoundException.java
+++ find-zipcode/src/main/java/br/com/findzipcode/exception/AddressNotFoundException.java
@@ -1,6 +1,7 @@
 package br.com.findzipcode.exception;
 
 /**
+ * Exception utilizada nos casos onde o Address buscado não existir
  * Created by Vinicius on 03/12/15.
  */
 public class AddressNotFoundException extends RuntimeException {
